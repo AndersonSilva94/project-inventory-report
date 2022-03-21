946efb21cc0a5fcd16aefc98332da0223c6873d6
Anderson Silva <andssilva.23@gmail.com>
2022-03-20 20:31:00 -0400

    feat (req 6.2, 6.5 e 6.8): :rocket: Cria uma classe chamada JsonImporter

---
+++ inventory_report/importer/json_importer.py
@@ -0,0 +1,15 @@
+"""
+Requisito 6.2, 6.5 e 6.8 - Passos a se seguir e lógica
+1 - Criar uma classe chamada JsonImporter, ela herdará a classe Importer
+2 - Seguirá toda a mesma lógica de implementação da classe CsvImporter
+"""
+from inventory_report.importer.importer import Importer
+from inventory_report.inventory.inventory import Inventory
+
+
+class JsonImporter(Importer):
+    def import_data(path_file):
+        if path_file.endswith('json'):
+            return Inventory.get_file_json(path_file)
+        else:
+            raise ValueError('Arquivo Inválido')
